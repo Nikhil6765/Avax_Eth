ed2b28b8a27786f98d2fb1aa1b978fce17793ed8
Nikhil6765 <98408521+Nikhil6765@users.noreply.github.com>
2023-07-14 20:38:29 +0530

    Create Memory.sol

---
+++ Memory.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+
+contract Memort{
+
+    function multiply(uint256 num) external pure returns(uint256) {
+        uint256 result = num * num;  return result;
+        
+    }
+
+
+}
